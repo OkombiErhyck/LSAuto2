a3637b68a4bd53f1b380694313d10e42909967af
Okombi Erhyck <okombie@gmail.com>
2023-03-10 18:47:51 +0200

    poate

--- api/index.js
+++ api/index.js
@@ -26,7 +26,14 @@ app.use(cors({
     origin: "http://localhost:3000",
 }));
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 
 async function uploadToS3(path, originalFilename, mimetype) {
