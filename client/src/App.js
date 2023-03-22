bc53132723c8cc2a761e8cffbe4a2727de056e4f
Okombi Erhyck <okombie@gmail.com>
2023-03-22 12:05:12 +0200

    rfdvdf

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import axios from "axios";
+import Navbar from './navbar';
+import Footer from './footer/footer';
+import Home from './home';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import { UserContextProvider } from './UserContext';
-import SplashScreen from "./SplashScreen";
+import SplashScreen from './SplashScreen';
 
-const About = lazy(() => import("./about"));
-const Login = lazy(() => import("./login"));
-const Signup = lazy(() => import("./signup"));
-const Navbar = lazy(() => import("./navbar"));
-const Footer = lazy(() => import("./footer/footer"));
-const Home = lazy(() => import("./home"));
-const Userpage = lazy(() => import("./userpage"));
-const Write = lazy(() => import("./write/Write"));
-const PlacesPage = lazy(() => import("./PlacesPage"));
-const IndexPage = lazy(() => import("./IndexPage"));
-const PlacePage = lazy(() => import("./PlacePage"));
-const ResetPassword = lazy(() => import("./reset-password"));
-const Despre = lazy(() => import("./despre"));
-const CookiePolicyPopup = lazy(() => import("./CookiePolicyPopup"));
-const Details = lazy(() => import("./details"));
+const About = lazy(() => import('./about'));
+const Login = lazy(() => import('./login'));
+const Signup = lazy(() => import('./signup'));
+const Userpage = lazy(() => import('./userpage'));
+const Write = lazy(() => import('./write/Write'));
+const PlacesPage = lazy(() => import('./PlacesPage'));
+const IndexPage = lazy(() => import('./IndexPage'));
+const PlacePage = lazy(() => import('./PlacePage'));
+const ResetPassword = lazy(() => import('./reset-password'));
+const Despre = lazy(() => import('./despre'));
+const CookiePolicyPopup = lazy(() => import('./CookiePolicyPopup'));
+const Details = lazy(() => import('./details'));
 
-axios.defaults.baseURL = "https://ls-auto2.vercel.app";
+axios.defaults.baseURL = 'https://ls-auto2.vercel.app';
 axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <UserContextProvider>
-        <Suspense fallback={<SplashScreen />}>
-          <Navbar />
-          <Routes>
-            <Route path="/details" element={<Details />} />
-            <Route path="/Write" element={<Write />} />
-            <Route path="/Write/:id" element={<Write />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/userpage" element={<Userpage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/PlacesPage" element={<PlacesPage />} />
-            <Route path="/IndexPage" element={<IndexPage />} />
-            <Route path="/place/:id" element={<PlacePage />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/despre" element={<Despre />} />
-          </Routes>
-          <Footer />
-        </Suspense>
+        <Navbar />
+        <Routes>
+          <Route path='/details' element={<Details />} />
+          <Route path='/Write' element={<Write />} />
+          <Route path='/Write/:id' element={<Write />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/userpage' element={<Userpage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/PlacesPage' element={<PlacesPage />} />
+          <Route path='/IndexPage' element={<IndexPage />} />
+          <Route path='/place/:id' element={<PlacePage />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/despre' element={<Despre />} />
+        </Routes>
+        <Footer />
       </UserContextProvider>
     </div>
   );
@@ -65,11 +63,13 @@ function Main() {
 
   return (
     <div>
-      {!isLoaded && <SplashScreen />}
-      {isLoaded && (
+      {isLoaded ? (
         <Suspense fallback={<SplashScreen />}>
           <App />
+          
         </Suspense>
+      ) : (
+        <SplashScreen />
       )}
     </div>
   );
