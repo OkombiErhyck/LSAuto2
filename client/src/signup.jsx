a426c50984d3399e2dd87d26e6f811ec6b445ade
Okombi Erhyck <okombie@gmail.com>
2023-04-11 09:23:05 +0300

    fghfhch

--- client/src/signup.jsx
+++ client/src/signup.jsx
@@ -1,17 +1,26 @@
-import React from "react";
 import "./signup.css";
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+import { useNavigate } from "react-router-dom";
 
 function Signup(){
-  const navigate = useNavigate(); // Instantiate useNavigate hook
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(null); // Add state for password error message
   
   async function registerUser(ev) {
     ev.preventDefault();
+    
+    // Check if password meets requirements
+    if (!isStrongPassword(password)) {
+      setPasswordError("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character");
+      return;
+    } else {
+      setPasswordError(null);
+    }
+    
     try{
       await axios.post("/register", {
         name,
@@ -21,11 +30,17 @@ function Signup(){
       
       alert("Registration successful. Now you can log in");
       
-      navigate("/login"); // Navigate to login page
+      navigate("/login");
       
     } catch (e) {
       alert("Registration failed. Please try again later");
     }
+  }
+  
+  // Function to check if password is strong
+  function isStrongPassword(password) {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+~])(?=.*[a-zA-Z]).{8,}$/;
+    return regex.test(password);
   }
 
   return(
@@ -40,8 +55,9 @@ function Signup(){
             <input type="email" placeholder="bun@exemplu.com"  value={email} onChange={ev => setEmail(ev.target.value)}/>
             <br></br><br></br>
             <input type="password" placeholder="parola"  value={password} onChange={ev => setPassword(ev.target.value)}/>
+            {passwordError && <p className="error">{passwordError}</p>} {/* Display error message if password is not strong */}
             <br></br> 
-            <button  className="loginbtn">Register<span></span></button>
+            <button className="loginbtn">Register<span></span></button>
           </form>   
         </div>    
       </div>
