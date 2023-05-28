0eae1802ffd64439d2a52672aced0112ca762eba
Okombi Erhyck <okombie@gmail.com>
2023-05-28 08:55:11 +0300

    vvdfvd

--- client/src/CookiePolicyPopup.js
+++ client/src/CookiePolicyPopup.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./cookie.css";
 
 const CookiePolicyPopup = () => {
   const [showPopup, setShowPopup] = useState(!document.cookie.includes('cookie_policy_accepted=true'));
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if user is already logged in based on a cookie or authentication status
+    const checkLoggedInStatus = () => {
+      const isLoggedInCookie = document.cookie.includes('isLoggedIn=true');
+      setIsLoggedIn(isLoggedInCookie);
+    };
+
+    checkLoggedInStatus();
+  }, []);
 
   const hidePopup = () => {
     setShowPopup(false);
     document.cookie = 'cookie_policy_accepted=true; path=/; max-age=31536000';
   };
 
-  const disableCrossSiteTracking = () => {
-    alert("To disable 'Prevent cross-site tracking', please follow these steps:\n\n1. Open your browser settings.\n2. Navigate to the 'Privacy' or 'Security' section.\n3. Find the option for 'Prevent cross-site tracking'.\n4. Disable the option if it's enabled.\n5. Refresh the page to continue browsing.");
+  const handleLogin = () => {
+    // Perform login logic here
+    // Set the login status in the cookie
+    document.cookie = 'isLoggedIn=true; path=/; max-age=86400'; // Set the cookie to expire in 24 hours
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    // Perform logout logic here
+    // Clear the login status cookie
+    document.cookie = 'isLoggedIn=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+    setIsLoggedIn(false);
   };
 
   return (
@@ -18,15 +39,14 @@ const CookiePolicyPopup = () => {
       <div className="cookie-policy-popup">
         <p>
           Acest website utilizează cookie-uri pentru a vă asigura cea mai bună experiență pe site-ul nostru.
-          {navigator.doNotTrack === "1" && (
-            <span>
-              {' '}
-              De asemenea, aveți activată funcția de „Prevent cross-site tracking” în browserul dvs.
-              <button onClick={disableCrossSiteTracking}>Doriți să dezactivați această funcție?</button>
-            </span>
-          )}
+         
           {' '}Aflați mai multe. <a href="/despre">aici</a>.
         </p>
+        {isLoggedIn ? (
+          <button onClick={handleLogout}>Logout</button>
+        ) : (
+          <button onClick={handleLogin}>Login</button>
+        )}
         <button onClick={hidePopup}>Am înțeles</button>
       </div>
     )
