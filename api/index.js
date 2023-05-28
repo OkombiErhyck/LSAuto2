1d93d47c527fe4b9c59920c66161496523039542
Okombi Erhyck <okombie@gmail.com>
2023-05-28 09:11:14 +0300

    dfgdfg

--- api/index.js
+++ api/index.js
@@ -87,25 +87,31 @@ app.post("/login", async (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
   res.header("Access-Control-Allow-Credentials", "true");
   res.set("Access-Control-Allow-Origin", "https://www.lsauto.ro");
-    const { email, password } = req.body;
-    const userDoc = await User.findOne({ email });
-    if (userDoc) {
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        jwt.sign({email:userDoc.email, id:userDoc._id, name:userDoc.name}, jwtSecret, {}, (err, token) => {
-             if (err) throw err;
-        
-        res.cookie("token", token, { sameSite: 'none', secure: true }).json(userDoc);
-
-    });
-
-      } else {
-        res.status(422).json("pass not ok");
-      }
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id, name: userDoc.name },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token, {
+            sameSite: "none",
+            secure: true,
+          }).json(userDoc);
+        }
+      );
     } else {
-      res.status(404).json("not found");
+      res.status(422).json("pass not ok");
     }
-  });
+  } else {
+    res.status(404).json("not found");
+  }
+});
+
 
   
   app.get("/profile", (req,res) => {
