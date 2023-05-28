b3f3695ad8550b82364b7b57d2a19fe416f22e9e
Okombi Erhyck <okombie@gmail.com>
2023-05-28 08:23:54 +0300

    fvfdv

--- client/src/CookiePolicyPopup.js
+++ client/src/CookiePolicyPopup.js
@@ -8,14 +8,26 @@ const CookiePolicyPopup = () => {
     setShowPopup(false);
     document.cookie = 'cookie_policy_accepted=true; path=/; max-age=31536000';
   };
-  
-     
+
+  const disableCrossSiteTracking = () => {
+    alert("To disable 'Prevent cross-site tracking', please follow these steps:\n\n1. Open your browser settings.\n2. Navigate to the 'Privacy' or 'Security' section.\n3. Find the option for 'Prevent cross-site tracking'.\n4. Disable the option if it's enabled.\n5. Refresh the page to continue browsing.");
+  };
 
   return (
     showPopup && (
       <div className="cookie-policy-popup">
-        <p>Acest website utilizează cookie-uri pentru a vă asigura cea mai bună experiență pe site-ul nostru. Aflați mai multe. <a href="/despre">aici</a>.</p>
-        <button onClick={hidePopup}>Am inteles</button>
+        <p>
+          Acest website utilizează cookie-uri pentru a vă asigura cea mai bună experiență pe site-ul nostru.
+          {navigator.doNotTrack === "1" && (
+            <span>
+              {' '}
+              De asemenea, aveți activată funcția de „Prevent cross-site tracking” în browserul dvs.
+              <button onClick={disableCrossSiteTracking}>Doriți să dezactivați această funcție?</button>
+            </span>
+          )}
+          {' '}Aflați mai multe. <a href="/despre">aici</a>.
+        </p>
+        <button onClick={hidePopup}>Am înțeles</button>
       </div>
     )
   );
