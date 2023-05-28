bd785a06a4c2ed15744c68f7faa650e4b95d741f
Okombi Erhyck <okombie@gmail.com>
2023-05-28 12:38:46 +0300

    fhfyb

--- client/src/userpage.jsx
+++ client/src/userpage.jsx
@@ -6,103 +6,107 @@ import Logout from "./images/logout.png";
 import "./userpage.css";
 import { UserContext } from "./UserContext";
 import { useContext } from "react";
- import axios from "axios";
+import axios from "axios";
 import { useState } from "react";
-import { Navigate, redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
+function Userpage() {
+  const { ready, user, setUser } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
 
-
-function Userpage(){
-    const {ready,user,setUser} = useContext(UserContext);
-    const [redirect,setRedirect] = useState(null);
-if (!ready) {
-
+  if (!ready) {
     return "loading...";
-}
+  }
 
-if (ready && !user) {
-    return <Navigate to={"/"}/>
-}
+  if (ready && !user) {
+    return <Navigate to={"/"} />;
+  }
 
-async function logout() {
-   await axios.post("/logout");
-   setUser(null)
-   setRedirect("/");
-    
-}
-if (redirect) {
-    return<Navigate to={redirect}/>
-}
+  async function logout() {
+    await axios.post("/logout");
+    setUser(null);
+    setRedirect("/");
+  }
 
-   return(
+  function vibrateButton() {
+    if ("vibrate" in navigator) {
+      navigator.vibrate(100); // Vibrate for 100 milliseconds
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
     <>
-     
-     <div className="userbox">
+      <div className="userbox">
         <div className="usercontent">
-          <h1>Bine ai venit,<span style={{color:"var(--main)"}}>  {user.name}</span> </h1>
+          <h1>
+            Bine ai venit,<span style={{ color: "var(--main)" }}> {user.name}</span>{" "}
+          </h1>
 
           <div className="details container">
-                <div className="row">
-                    
-                    <div className="col-lg-4 col-xs-6">
-                       <div className="box card-body p-0  shadow-sm mb-5">
-                           
-                           <div className="box_content">
-                           <a href="/placespage">
-                           <img src={PlacesPage} className="img-fluid" alt=""/>
-                            <button  className="btn1">Anunturile Mele</button>
-                            </a>
-                           </div>
-                       </div>
-                    </div>
-
-
-                    <div className="col-lg-4 col-xs-6">
-                       <div className="box card-body p-0  shadow-sm mb-5">
-                          
-                           <div className="box_content">
-                           <a href="/write">
-                           <img src={Anunt} className="img-fluid" alt=""/>
-                          
-                               <button className="btn1">Adauga un Anunt</button>
-                           </a>
-                           </div>
-                       </div>
-                    </div>
-
-                    <div className="col-lg-4 col-xs-6">
-                       <div className="box card-body p-0  shadow-sm mb-5">
-                          
-                           <div className="box_content">
-                           <a href="/IndexPage">
-                           <img src={toate} className="img-fluid" alt=""/>
-                          
-                               <button className="btn1">Toate Anunturile</button>
-                           </a>
-                           </div>
-                       </div>
-                    </div>
-
-
-                    <div className="col-lg-4 col-xs-6">
-                       <div onClick={logout} className="box card-body p-0  shadow-sm mb-5">
-                           
-                           <div className="box_content">
-                           <img src={Logout} className="img-fluid" alt=""/>
-                            <span onClick={logout} className="btn1">Iesire din Cont</span>
-                           </div>
-                       </div>
-                    </div>
-
+            <div className="row">
+              <div className="col-lg-4 col-xs-6">
+                <div className="box card-body p-0 shadow-sm mb-5">
+                  <div className="box_content">
+                    <a href="/placespage">
+                      <img src={PlacesPage} className="img-fluid" alt="" />
+                      <button className="btn1" onClick={vibrateButton}>
+                        Anunturile Mele
+                      </button>
+                    </a>
+                  </div>
                 </div>
-            </div>
-     
-        </div>
-        
-    </div>
-</>
-     );
-};
+              </div>
 
+              <div className="col-lg-4 col-xs-6">
+                <div className="box card-body p-0 shadow-sm mb-5">
+                  <div className="box_content">
+                    <a href="/write">
+                      <img src={Anunt} className="img-fluid" alt="" />
+                      <button className="btn1" onClick={vibrateButton}>
+                        Adauga un Anunt
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-xs-6">
+                <div className="box card-body p-0 shadow-sm mb-5">
+                  <div className="box_content">
+                    <a href="/IndexPage">
+                      <img src={toate} className="img-fluid" alt="" />
+                      <button className="btn1" onClick={vibrateButton}>
+                        Toate Anunturile
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-xs-6">
+                <div
+                  onClick={() => {
+                    vibrateButton();
+                    logout();
+                  }}
+                  className="box card-body p-0 shadow-sm mb-5"
+                >
+                  <div className="box_content">
+                    <img src={Logout} className="img-fluid" alt="" />
+                    <span className="btn1">Iesire din Cont</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Userpage;
