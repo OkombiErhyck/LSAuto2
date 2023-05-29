3170b18d8b71a08be2e2385dfb39b23cd6c7d8c2
Okombi Erhyck <okombie@gmail.com>
2023-05-29 09:59:56 +0300

    dgdrggerg

--- client/src/PlacesPage.jsx
+++ client/src/PlacesPage.jsx
@@ -11,52 +11,73 @@ export default function PlacesPage() {
 
   useEffect(() => {
     axios.get('/user-places').then(({ data }) => {
-      setPlaces(data);
+      const placesWithClickCount = data.map(place => ({ ...place, clickCount: 0 }));
+      setPlaces(placesWithClickCount);
     });
   }, []);
 
   const handleDelete = (event, id) => {
     event.preventDefault();
     axios.delete(`/places/${id}`).then(() => {
-      setPlaces((prevPlaces) => prevPlaces.filter((place) => place._id !== id));
+      setPlaces(prevPlaces => prevPlaces.filter(place => place._id !== id));
     });
   };
-  
+
+  const handlePlaceClick = (id) => {
+    setPlaces(prevPlaces =>
+      prevPlaces.map(place => {
+        if (place._id === id) {
+          return {
+            ...place,
+            clickCount: place.clickCount + 1
+          };
+        }
+        return place;
+      })
+    );
+  };
 
   return (
     <>
       <div className="top"></div>
 
-      <div className="main2"> 
-       
+      <div className="main2">
         <div className="container">
           <div className="details container">
             <div className="row row-cols-1 row-cols-md-3 g-4">
-              {places.length > 0 && places.map(place => ( 
-                <Link className="link-no-underline" to={"/write/"+place._id}>
-                  <div className="col ">
-                    <div className="box card-body p-0  shadow-sm mb-5">
-                      {place.photos.length > 0 && (
-                        <Image src={place.photos[0]} className="img-fluid" style={{height: "270px", width: "100%", objectFit: "cover"}}/>
-                      )}
-                      <div className="box_content">
-                     <h4> {place.marca} {place.model}</h4>
-                     <div className="row pl-2 pr-2">
-    <div > 
-      {place.putere} cp | {place.anul} | {place.km} km     <h5> {place.title}€</h5>
-  </div>
- 
-                     
-
-
-  <button style={{background : "#cccccc00", color : "var(--main)"}} className="btn1">Detalii</button>
-                         
-                        <button style={{color : "red"}} className="btn1" onClick={(event) => handleDelete(event, place._id)}>Sterge</button>
-                     </div>
+              {places.length > 0 &&
+                places.map(place => (
+                  <Link className="link-no-underline" to={"/write/" + place._id} key={place._id}>
+                    <div className="col">
+                      <div className="box card-body p-0  shadow-sm mb-5" onClick={() => handlePlaceClick(place._id)}>
+                        {place.photos.length > 0 && (
+                          <Image
+                            src={place.photos[0]}
+                            className="img-fluid"
+                            style={{ height: "270px", width: "100%", objectFit: "cover" }}
+                          />
+                        )}
+                        <div className="box_content">
+                          <h4>
+                            {place.marca} {place.model}
+                          </h4>
+                          <div className="row pl-2 pr-2">
+                            <div>
+                              {place.putere} cp | {place.anul} | {place.km} km <h5>{place.title}€</h5>
+                            </div>
+                            <button style={{ background: "#cccccc00", color: "var(--main)" }} className="btn1">
+                              Detalii
+                            </button>
+                            <button style={{ color: "red" }} className="btn1" onClick={(event) => handleDelete(event, place._id)}>
+                              Sterge
+                            </button>
+                          </div>
+                          <p>Clicks: {place.clickCount}</p> {/* Display the click count */}
+                        </div>
+                      </div>
                     </div>
-                  </div></div>
-                </Link>
-              ))}
+                  </Link>
+                ))}
             </div>
           </div>
         </div>
