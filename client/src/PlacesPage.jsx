29ef378a9f39cf8753a2c2785124dae3b1e78271
Okombi Erhyck <okombie@gmail.com>
2023-05-29 12:18:04 +0300

    vfdvdf

--- client/src/PlacesPage.jsx
+++ client/src/PlacesPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Myadds.css';
 import Image from './image';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { incrementClickCount } from './actions';
 
