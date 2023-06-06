2b7cad7713383150ca4591109b5c8987294dbd00
Okombi Erhyck <okombie@gmail.com>
2023-06-06 10:16:59 +0300

    vdfvdfv

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,24 +1,53 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import {data} from "autoprefixer";
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}) {
-  const [user,setUser] = useState(null);
-  const [ready,setReady] = useState(false);
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
     if (!user) {
-      axios.get('/profile').then(({data}) => {
-        setUser(data);
-        setReady(true);
-      });
+      axios
+        .get('/profile', { withCredentials: true })
+        .then(({ data }) => {
+          setUser(data);
+          setReady(true);
+        })
+        .catch(() => {
+          setUser(null);
+          setReady(true);
+        });
     }
   }, []);
 
-  
+  const login = (email, password) => {
+    axios
+      .post("/login", { email, password }, { withCredentials: true })
+      .then(({ data }) => {
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+        setUser(null);
+      });
+  };
+
+  const logout = () => {
+    axios
+      .post("/logout", {}, { withCredentials: true })
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.error("Logout error:", error);
+        setUser(null);
+      });
+  };
+
   return (
-    <UserContext.Provider value={{user,setUser,ready}}>
+    <UserContext.Provider value={{ user, setUser, ready, login, logout }}>
       {children}
     </UserContext.Provider>
   );
