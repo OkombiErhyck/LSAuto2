20aa82f842e6272a76e344e57363a44368515117
Okombi Erhyck <okombie@gmail.com>
2023-07-05 14:02:50 +0300

    dfdf

--- client/src/footer/footer.jsx
+++ client/src/footer/footer.jsx
@@ -1,18 +1,11 @@
 import "./footer.css";
-import "../footer2.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTiktok, faFacebook, faIdbadge , faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
-
 const Footer = () => {
-    
     return (
         <>
         
            <footer>
             
-        
+                   
                 
                 <p>Copyright &copy; LSAuto | 2023 All rights reserved  <a href="/despre" className="nav-link">
                     politici
