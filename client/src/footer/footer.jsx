07557b010c138f22358cff9fb403fd97d5a80574
Okombi Erhyck <okombie@gmail.com>
2023-07-05 13:53:47 +0300

    fgd

--- client/src/footer/footer.jsx
+++ client/src/footer/footer.jsx
@@ -6,40 +6,17 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 const Footer = () => {
-    
+    const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/contact'); // Replace '/contact' with the actual route to your contact component
+  };
     return (
         <>
         
            <footer>
             
-           <div className="container py-5" style={{marginBottom: "-8%"}}>
-     
-     <br/><br/><br/><br/>
-     <div className="row">
-       <ul>
-         <li>
-         <a href="https://www.tiktok.com/@lsauto.ro">
-              <FontAwesomeIcon style={{marginTop: "19px"}} icon={faTiktok} className="fa" />
-              <span>  TikTok</span>
-            </a>
-
-         </li>
-         <li>
-           <a href="/contact">
-           <FontAwesomeIcon style={{marginTop: "19px"}} icon={faEnvelope } className="fa" />
-             <span>  Contact</span>
-           </a>
-         </li>
-         <li>
-           <a href="https://www.instagram.com/lsauto.ro/">
-           <FontAwesomeIcon style={{marginTop: "19px"}} icon={faInstagram} className="fa" />
-             <span>  Instagram</span>
-           </a>
-         </li>
-       </ul>
-     </div>
-   </div>
-                
+            
                 <p>Copyright &copy; LSAuto | 2023 All rights reserved  <a href="/despre" className="nav-link">
                     politici
                   </a></p>
