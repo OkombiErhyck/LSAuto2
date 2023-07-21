4fcf1f9e2d5d3fc7c0f0a030efb148c1b9f13bc6
Okombi Erhyck <okombie@gmail.com>
2023-07-21 12:01:49 +0300

    hghgj

--- client/src/despre.jsx
+++ client/src/despre.jsx
@@ -49,50 +49,50 @@ Cookies persistente - aceste cookies sunt stocate în browserul dispozitivului d
 
 <h1 className="font-weight-bold">Politici pentru publicarea anunturilor</h1>
 
-<p>1. Introducere
+<p>1. Introducere</p>
 
-Această Politică privind Anunțurile Publicate de Utilizatori reglementează procesul de trimitere, publicare și afișare a anunțurilor generate de utilizatori pe platforma lsauto.ro . Prin utilizarea Website-ului și trimiterea anunțurilor, utilizatorii sunt de acord să respecte această politică.
+<p>Această Politică privind Anunțurile Publicate de Utilizatori reglementează procesul de trimitere, publicare și afișare a anunțurilor generate de utilizatori pe platforma lsauto.ro. Prin utilizarea Website-ului și trimiterea anunțurilor, utilizatorii sunt de acord să respecte această politică.</p>
 
-2. Ghiduri privind Conținutul Anunțurilor
+<p>2. Ghiduri privind Conținutul Anunțurilor</p>
 
-2.1. Informații Exacte despre Vehicule: Utilizatorii trebuie să furnizeze informații precise și veridice despre vehiculele pe care le vând în anunțurile lor. Conținutul înșelător sau păcălelile, inclusiv prețuri sau specificații false ale vehiculelor, sunt strict interzise.
+<p>2.1. Informații Exacte despre Vehicule: Utilizatorii trebuie să furnizeze informații precise și veridice despre vehiculele pe care le vând în anunțurile lor. Conținutul înșelător sau păcălelile, inclusiv prețuri sau specificații false ale vehiculelor, sunt strict interzise.</p>
 
-2.2. Conținut Interzis: Anunțurile nu trebuie să conțină conținut care promovează produse sau servicii ilegale, materiale pentru adulți sau orice conținut ofensator sau dăunător. Conținutul legat de arme, droguri, jocuri de noroc sau alte categorii restricționate este strict interzis.
+<p>2.2. Conținut Interzis: Anunțurile nu trebuie să conțină conținut care promovează produse sau servicii ilegale, materiale pentru adulți sau orice conținut ofensator sau dăunător. Conținutul legat de arme, droguri, jocuri de noroc sau alte categorii restricționate este strict interzis.</p>
 
-2.3. Drepturi de Proprietate Intelectuală: Utilizatorii sunt responsabili să se asigure că dețin drepturile și permisiunile necesare pentru a publica conținutul pe care îl trimit. Anunțurile nu trebuie să încalce drepturile de proprietate intelectuală ale altora.
+<p>2.3. Drepturi de Proprietate Intelectuală: Utilizatorii sunt responsabili să se asigure că dețin drepturile și permisiunile necesare pentru a publica conținutul pe care îl trimit. Anunțurile nu trebuie să încalce drepturile de proprietate intelectuală ale altora.</p>
 
-2.4. Stare și Istoric al Vehiculului: Utilizatorii trebuie să prezinte cu exactitate starea vehiculelor pe care le vând. Orice defecte semnificative cunoscute sau istoric al accidentelor trebuie menționate în anunțuri.
+<p>2.4. Stare și Istoric al Vehiculului: Utilizatorii trebuie să prezinte cu exactitate starea vehiculelor pe care le vând. Orice defecte semnificative cunoscute sau istoric al accidentelor trebuie menționate în anunțuri.</p>
 
-2.5. Relevanță: Anunțurile trebuie să fie relevante pentru platforma lsauto.ro, concentrându-se exclusiv pe vehicule și produse/servicii legate de domeniul auto.
+<p>2.5. Relevanță: Anunțurile trebuie să fie relevante pentru platforma lsauto.ro, concentrându-se exclusiv pe vehicule și produse/servicii legate de domeniul auto.</p>
 
-3. Siguranța Utilizatorilor
+<p>3. Siguranța Utilizatorilor</p>
 
-3.1. Informații Personale: Utilizatorii trebuie să evite furnizarea de informații personale precum date de contact sau adrese în anunțurile lor. Canalurile de comunicare private oferite de platformă ar trebui utilizate în acest scop.
+<p>3.1. Informații Personale: Utilizatorii trebuie să evite furnizarea de informații personale precum date de contact sau adrese în anunțurile lor. Canalurile de comunicare private oferite de platformă ar trebui utilizate în acest scop.</p>
 
-3.2. Avertizări de Siguranță: Utilizatorii sunt încurajați să ofere avertizări și sfaturi pentru siguranța potențialilor cumpărători, cum ar fi evitarea metodelor frauduloase de plată sau efectuarea de teste de conducere sigure.
+<p>3.2. Avertizări de Siguranță: Utilizatorii sunt încurajați să ofere avertizări și sfaturi pentru siguranța potențialilor cumpărători, cum ar fi evitarea metodelor frauduloase de plată sau efectuarea de teste de conducere sigure.</p>
 
-4. Moderare și Aprobare a Conținutului
+<p>4. Moderare și Aprobare a Conținutului</p>
 
-4.1. Moderare: Administratorii Website-ului vor revizui și modera toate anunțurile înainte de a fi publicate, pentru a se asigura că respectă această politică.
+<p>4.1. Moderare: Administratorii Website-ului vor revizui și modera toate anunțurile înainte de a fi publicate, pentru a se asigura că respectă această politică.</p>
 
-4.2. Aprobarea Conținutului: Anunțurile care respectă ghidurile politicii vor fi aprobate pentru a fi afișate pe Website. Cele care încalcă politica nu vor fi publicate sau vor fi eliminate.
+<p>4.2. Aprobarea Conținutului: Anunțurile care respectă ghidurile politicii vor fi aprobate pentru a fi afișate pe Website. Cele care încalcă politica nu vor fi publicate sau vor fi eliminate.</p>
 
-5. Mecanism de Raportare
+<p>5. Mecanism de Raportare</p>
 
-5.1. Raportarea Încălcărilor: Utilizatorii și vizitatorii Website-ului sunt încurajați să raporteze orice anunț pe care cred că încalcă această politică. Mecanismul de raportare va fi disponibil și accesibil pe Website.
+<p>5.1. Raportarea Încălcărilor: Utilizatorii și vizitatorii Website-ului sunt încurajați să raporteze orice anunț pe care cred că încalcă această politică. Mecanismul de raportare va fi disponibil și accesibil pe Website.</p>
 
-5.2. Gestionarea Rapoartelor: Anunțurile raportate vor fi revizuite prompt de către administratorii Website-ului, iar acțiunile adecvate vor fi întreprinse, inclusiv eliminarea sau modificarea conținutului.
+<p>5.2. Gestionarea Rapoartelor: Anunțurile raportate vor fi revizuite prompt de către administratorii Website-ului, iar acțiunile adecvate vor fi întreprinse, inclusiv eliminarea sau modificarea conținutului.</p>
 
-6. Conformitate și Modificări
+<p>6. Conformitate și Modificări</p>
 
-6.1. Responsabilitatea Utilizatorilor: Utilizatorii sunt responsabili să se familiarizeze cu această politică și să se asigure că anunțurile lor respectă ghidurile impuse.
+<p>6.1. Responsabilitatea Utilizatorilor: Utilizatorii sunt responsabili să se familiarizeze cu această politică și să se asigure că anunțurile lor respectă ghidurile impuse.</p>
 
-6.2. Modificări ale Politicii: Website-ul își rezervă dreptul de a actualiza sau modifica această politică în orice moment. Utilizatorii vor fi informați cu privire la eventualele modificări semnificative ale politicii.
+<p>6.2. Modificări ale Politicii: Website-ul își rezervă dreptul de a actualiza sau modifica această politică în orice moment. Utilizatorii vor fi informați cu privire la eventualele modificări semnificative ale politicii.</p>
 
-7. Concluzie
+<p>7. Concluzie</p>
 
-Politica privind Anunțurile Publicate de Utilizatori își propune să ofere un mediu fiabil și de încredere utilizatorilor pentru a cumpăra și vinde vehicule pe lsauto.ro. Prin respectarea acestor ghiduri, utilizatorii contribuie la o experiență pozitivă pentru toți utilizatorii platformei. Dacă oricare utilizator are întrebări sau nelămuriri cu privire la această politică sau la anunțurile lor, pot contacta administratorii Website-ului pentru asistență.</p>
-    </div>
+<p>Politica privind Anunțurile Publicate de Utilizatori își propune să ofere un mediu fiabil și de încredere utilizatorilor pentru a cumpăra și vinde vehicule pe lsauto.ro. Prin respectarea acestor ghiduri, utilizatorii contribuie la o experiență pozitivă pentru toți utilizatorii platformei. Dacă oricare utilizator are întrebări sau nelămuriri cu privire la această politică sau la anunțurile lor, pot contacta administratorii Website-ului pentru asistență.</p>
+  </div>
   </div>
    
    
